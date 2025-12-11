43088087fbaeee49a5975ddef2c3046491e86cb8
Estecka <estecka@galacsys.com>
2025-12-11 21:33:05 +0100

    Fix miwin target

--- src/main/java/fr/estecka/preferredgamerules/mixin/GameRulesMixin.java
+++ src/main/java/fr/estecka/preferredgamerules/mixin/GameRulesMixin.java
@@ -5,9 +5,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fr.estecka.preferredgamerules.PrefRulesMod;
+import net.minecraft.world.rule.GameRules;
 
 
-@Mixin(GameRulesMixin.class)
+@Mixin(GameRules.class)
 public class GameRulesMixin
 {
 	@Inject( method="<clinit>", at=@At("HEAD") )
