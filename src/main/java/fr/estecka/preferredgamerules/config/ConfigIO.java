fd94e52f4cc85eab1cb4a314c412902bf6d539b3
Estecka <estecka@galacsys.com>
2025-12-11 22:18:16 +0100

    Updated ConfigIO

--- src/main/java/fr/estecka/preferredgamerules/config/ConfigIO.java
+++ src/main/java/fr/estecka/preferredgamerules/config/ConfigIO.java
@@ -2,7 +2,8 @@ package fr.estecka.preferredgamerules.config;
 
 
 /*
- * Copyright (c) 2023 Estecka
+ * # The MIT License (MIT)
+ * Copyright (c) 2024 Estecka
  *
  * Permission is hereby granted, free of charge,  to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -41,6 +42,7 @@ import net.fabricmc.loader.api.FabricLoader;
 
 public class ConfigIO
 {
+	static public final String VERSION = "1.3";
 	static private final Logger LOGGER = LoggerFactory.getLogger("ConfigIO");
 
 	private final File file;
@@ -67,7 +69,8 @@ public class ConfigIO
 	implements ICodec
 	{
 		/**
-		 * Defines a set of properties that will encoded into the config file
+		 * Defines the set of properties that will be looked for in the config
+		 * file.
 		 */
 		public abstract Map<String, Property<?>> GetProperties();
 
@@ -101,6 +104,7 @@ public class ConfigIO
 		static public Property<String>  String (Supplier<String>  getter, Consumer<String>  setter) { return new Property<>(getter, setter, s->s, s->s); }
 		static public Property<Integer> Integer(Supplier<Integer> getter, Consumer<Integer> setter) { return new Property<>(getter, setter, Integer::parseInt, i->i.toString()); }
 		static public Property<Float>   Float  (Supplier<Float>   getter, Consumer<Float>   setter) { return new Property<>(getter, setter, Float::parseFloat, f->f.toString()); }
+		static public Property<Double>  Double (Supplier<Double>  getter, Consumer<Double>  setter) { return new Property<>(getter, setter, Double::parseDouble, f->f.toString()); }
 		static public Property<Boolean> Boolean(Supplier<Boolean> getter, Consumer<Boolean> setter) { return new Property<>(getter, setter, Boolean::parseBoolean, b->b.toString()); }
 
 		public void Decode(String s){ this.setter.accept(this.parser.apply(s)); }
@@ -110,17 +114,31 @@ public class ConfigIO
 	/**
 	 * @param config A config object prefilled  with default values. This object
 	 * will be filled with the new values from the config file.
+	 * If the file  doesn't  exist, it  will  be initialized  with the  provided
+	 * config object.
 	 * @throws IOException
 	 */
 	public void	GetOrCreate(ICodec config)
 	throws IOException
 	{
-		if (!this.file.exists())
+		if (!this.GetIfExists(config))
 			this.Write(config);
-		else {
+	}
+
+	/**
+	 * Read the file into the given config object. Does nothing otherwise.
+	 * @return Whether the file exists.
+	 */
+	public boolean GetIfExists(ICodec config)
+	throws IOException
+	{
+		if (this.file.exists()){
 			var properties = ReadFile(this.file);
 			config.Decode(properties);
+			return true;
 		}
+		else
+			return false;
 	}
 
 	public void Write(ICodec config)
@@ -163,6 +181,7 @@ public class ConfigIO
 	static public void	WriteFile(File file, Map<String,String> properties)
 	throws IOException
 	{
+		file.getParentFile().mkdirs();
 		try (FileOutputStream out = new FileOutputStream(file, false))
 		{
 			final PrintWriter writer = new PrintWriter(out);
